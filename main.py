86a2768b323b2994fd7990157daeb61694243cb5
Aaron0720 <60797636+Aaron0720@users.noreply.github.com>
2020-03-18 11:19:20 -0400

    part2

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 25 20:33:47 2020
+import argparse
+from time import sleep
+from src.api1 import api
 
-@author: Yuki
-"""
-from sys import argv
-from src import api1
+
 if __name__=="__main__":
-    input_fn = argv[1]
-    try:
-        output_fn=argv[2]
-    except Exception:
-        output_fn='output.txt'
-    with open(input_fn,'r') as fh:
-        for line in fh:
-            line = line.strip('\n')
-            page_size,num_pages = line.split(' ')
-            page_size=int(page_size)
-            num_pages=int(num_pages)
-            api1.api(page_size,num_pages,output_fn)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--page_size',type=int)
+    parser.add_argument('--num_pages',type=int,default=None)
+    parser.add_argument('--output',default=None)
+    parser.add_argument('--elastic',type=bool,default=False)
+    args = parser.parse_args()
+
+
+    api(args.page_size,args.num_pages,args.output,args.elastic)
+
     
--- src/api1.py
+++ src/api1.py
@@ -1,15 +1,51 @@
-
-
 import os
 import json 
 from sodapy import Socrata
-def api(page_size,num_pages,output_fn):
-    APP_KEY=os.environ.get('APP_KEY')
-    client = Socrata("data.cityofnewyork.us",APP_KEY)
-    for i in range(num_pages):
-        ncdata=client.get("nc67-uf89",limit=page_size,offset=page_size*i)
-        with open(output_fn,'a') as fw:
-            print(ncdata)
-            output=json.dumps(ncdata)
-            fw.write(output+'\n')
+from requests import get
+from time import sleep
+from datetime import datetime,date
+from elasticsearch import Elasticsearch
 
+APP_KEY=os.environ.get('APP_KEY')
+client = Socrata("data.cityofnewyork.us",APP_KEY)
+
+def create_and_update_index(index_name):
+    es = Elasticsearch()
+    try:
+        es.indices.create(index=index_name)
+    except:
+        pass
+    return es
+
+def result_format(result):
+    for key, value in result.items():
+        if 'amount' in key:
+            result[key] = float(value)
+        elif 'date' in key:
+            result[key] = datetime.strptime(result[key], '%m/%d/%Y').date()
+        elif 'number' in key:
+            result[key]=int(value)
+
+def push_result(result, es, index):
+    result_format(result)
+    res = es.index(index=index, body=result,id=result['summons_number']) 
+
+
+
+def api(page_size, num_pages, output,elastic):
+    records = []
+    rows = int(client.get('nc67-uf89',select='COUNT(*)')[0]['COUNT'])
+
+    if not num_pages:
+        num_pages = rows // page_size+1
+    
+    if elastic:
+        es = create_and_update_index('nycvp')
+
+    for i in range(num_pages):
+        ncdata=client.get('nc67-uf89',limit=page_size,offset=page_size*i)
+        for result in ncdata:
+            with open(output,'a') as temp:
+                temp.write(json.dump(result)+'\n')
+            push_result(result,es,'nycvp')
+    
